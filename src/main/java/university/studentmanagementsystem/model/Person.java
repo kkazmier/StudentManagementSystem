452e9cdf3dd7c4553e6063270e12b46727e6d53e
kkazmier <dev.karol.kazmierczak@gmail.com>
2024-11-15 19:06:33 +0100

    Add constructor, getters/setters.

--- src/main/java/university/studentmanagementsystem/model/Person.java
+++ src/main/java/university/studentmanagementsystem/model/Person.java
@@ -1,5 +1,22 @@
 package university.studentmanagementsystem.model;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
     
+    public int getAge() {
+        return age;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/university/studentmanagementsystem/model/Student.java
+++ src/main/java/university/studentmanagementsystem/model/Student.java
@@ -1,5 +1,34 @@
 package university.studentmanagementsystem.model;
 
 public class Student extends Person {
+    private double grade;
+    private String studentID;
     
+    public void Student(String name, int age, double grade) {
+        super.setName(name);
+        super.setAge(age);
+        this.grade = grade;
+    }
+    
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    
+    @Override
+    public int getAge() {
+        return super.getAge();
+    }
+    
+    public double getGrade() {
+        return grade;
+    }
+    
+    public String GetStudentId() {
+        return studentID;
+    }
+    
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
 }
